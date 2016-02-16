06315d8a60bae31b95ab2284d57c16678ad6d9e2
Justin Hallier <justin.hallier+git@gmail.com>
2016-02-16 15:15:16 +1300

    Added listener to change summary of interval when user changes it

--- app/src/main/java/com/jallier/kitchentimer/Preferences.java
+++ app/src/main/java/com/jallier/kitchentimer/Preferences.java
@@ -1,7 +1,10 @@
 package com.jallier.kitchentimer;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 
 
@@ -14,11 +17,42 @@ public class Preferences extends AppCompatActivity {
                 .commit();
     }
 
-    public static class Display extends PreferenceFragment {
+    public static class Display extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
+        String defaultSummary;
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.preferences);
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity().getApplicationContext());
+            Preference interval = findPreference(MainActivity.PREF_SPEAK_INTERVAL);
+            defaultSummary = interval.getSummary().toString();
+            interval.setSummary(""
+                    + prefs.getString(MainActivity.PREF_SPEAK_INTERVAL, "1")
+                    + " minutes - "
+                    + defaultSummary);
+        }
+
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+            if (key.equals(MainActivity.PREF_SPEAK_INTERVAL)) {
+                Preference interval = findPreference(key);
+                interval.setSummary(""
+                        + sharedPreferences.getString(MainActivity.PREF_SPEAK_INTERVAL, "1")
+                        + " minutes - "
+                        + defaultSummary);
+            }
+        }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+            getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+        }
+
+        @Override
+        public void onPause() {
+            super.onPause();
+            getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
         }
     }
 }
